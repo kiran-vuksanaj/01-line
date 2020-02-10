103714dd4cb34677893d06ed5cd6728fbf734303
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2020-02-09 20:04:44 -0500

    working octant 2/6,vertical line

--- draw.c
+++ draw.c
@@ -15,6 +15,7 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
   int A = y1 - y0;
   int B = x0 - x1;
   if( x1 - x0 >= y1 - y0){
+    // slope between -1 and 1
     if( y1 > y0 ){
       // CASE octant I (octant V)
       int d = 2*A + B;
@@ -46,5 +47,22 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
     }
   } else {
     // slope magnitude greater than 1
+    if( y1 > y0 ){
+      // CASE octant II (octant VI)
+      int d = A + 2*B;
+      A *= 2;
+      B *= 2;
+      while( y0 < y1 ){
+	plot(s,c,x0,y0);
+	if( d < 0 ){
+	  x0++;
+	  d += A;
+	}
+	y0++;
+	d += B;
+      }
+    }else {
+
+    }
   }
 }
